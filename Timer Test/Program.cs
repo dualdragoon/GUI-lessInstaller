4a18f917ddf457ddb832536c759526138ab5457c
Dualdragoon <hosleraaron06@gmail.com>
2016-12-19 19:08:04 -0500

    Updated.

--- Timer Test/Program.cs
+++ Timer Test/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Timers;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+//using System.Windows.Forms;
 
 namespace Timer_Test
 {
@@ -15,7 +15,7 @@ namespace Timer_Test
 
         static void Main(string[] args)
         {
-            System.Timers.Timer t = new System.Timers.Timer(5000);
+            Timer t = new System.Timers.Timer(10000);
             t.Elapsed += new ElapsedEventHandler(OnElapsed);
 
             Parallel.Invoke(() => t.Start(), () => s.Start());
@@ -23,15 +23,15 @@ namespace Timer_Test
             while (t.Enabled)
             {
                 string arrow = "";
-                double percent = ((s.ElapsedMilliseconds / t.Interval) * 100);
+                decimal percent = (decimal)((s.ElapsedMilliseconds / t.Interval) * 100);
 
                 for (int i = 0; i < 40; i++)
                 {
-                    if (i < (int)(percent / 2.5) - 1)
+                    if (i < (int)(percent / 2.5m) - 1)
                     {
                         arrow += "=";
                     }
-                    else if (i == (int)(percent / 2.5) - 1)
+                    else if (i == (int)(percent / 2.5m) - 1)
                     {
                         arrow += ">";
                     }
@@ -48,8 +48,7 @@ namespace Timer_Test
 
         static void OnElapsed(object sender, ElapsedEventArgs args)
         {
-            ((System.Timers.Timer)sender).Stop();
-            s.Stop();
+            Parallel.Invoke(() => ((Timer)sender).Stop(), () => s.Stop());
             Console.WriteLine("\nDone!");
         }
     }
